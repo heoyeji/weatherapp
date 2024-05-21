68e49c64ba79cd9bed009a35419f9382e3d6c875
heoyeji <dpwl30226@gmail.com>
2024-05-21 15:33:29 +0900

    weather

---
+++ custom1.js
@@ -0,0 +1,54 @@
+const _0x5ab560 = _0x31a2;
+function _0x31a2(_0x4b99b4, _0x11d483) {
+  const _0x373b62 = _0x373b();
+  return (
+    (_0x31a2 = function (_0x31a297, _0x2f14c9) {
+      _0x31a297 = _0x31a297 - 0x80;
+      let _0x368961 = _0x373b62[_0x31a297];
+      return _0x368961;
+    }),
+    _0x31a2(_0x4b99b4, _0x11d483)
+  );
+}
+function _0x373b() {
+  const _0x9d04bc = [
+    "3b65f9d35ac8d71d03070db7f1cc51de",
+    "1460PjSlDM",
+    "2095177fXPtrn",
+    "8RzPFpV",
+    "794440WlMuzK",
+    "2172156RbflXr",
+    "13059neFhoD",
+    "4825VoQkWL",
+    "110Tnrcvn",
+    "268362vStaWR",
+    "1256hMGUeX",
+    "156999RCBApJ",
+  ];
+  _0x373b = function () {
+    return _0x9d04bc;
+  };
+  return _0x373b();
+}
+(function (_0x46a827, _0x485e5e) {
+  const _0x585e6b = _0x31a2,
+    _0x242d37 = _0x46a827();
+  while (!![]) {
+    try {
+      const _0x30f7b3 =
+        parseInt(_0x585e6b(0x85)) / 0x1 +
+        (parseInt(_0x585e6b(0x8b)) / 0x2) * (parseInt(_0x585e6b(0x87)) / 0x3) +
+        (-parseInt(_0x585e6b(0x89)) / 0x4) *
+          (-parseInt(_0x585e6b(0x83)) / 0x5) +
+        parseInt(_0x585e6b(0x81)) / 0x6 +
+        -parseInt(_0x585e6b(0x8a)) / 0x7 +
+        (parseInt(_0x585e6b(0x86)) / 0x8) * (parseInt(_0x585e6b(0x82)) / 0x9) +
+        (parseInt(_0x585e6b(0x80)) / 0xa) * (-parseInt(_0x585e6b(0x84)) / 0xb);
+      if (_0x30f7b3 === _0x485e5e) break;
+      else _0x242d37["push"](_0x242d37["shift"]());
+    } catch (_0x126406) {
+      _0x242d37["push"](_0x242d37["shift"]());
+    }
+  }
+})(_0x373b, 0x4f971);
+const config = { apikey: _0x5ab560(0x88) };
